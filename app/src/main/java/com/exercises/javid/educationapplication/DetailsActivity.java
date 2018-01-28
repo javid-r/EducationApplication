822f7315cf2b7b362409a1c8b7c7f6de6efd0f21
javid-r <javid.rafiei@yahoo.com>
2018-01-28 03:43:35 +0330

    v1

--- app/src/main/java/com/exercises/javid/educationapplication/DetailsActivity.java
+++ app/src/main/java/com/exercises/javid/educationapplication/DetailsActivity.java
@@ -1,15 +1,10 @@
 package com.exercises.javid.educationapplication;
 
 import android.app.Activity;
-import android.app.ProgressDialog;
-import android.content.Intent;
 import android.media.MediaPlayer;
 import android.os.Bundle;
-import android.support.design.widget.FloatingActionButton;
-import android.support.design.widget.Snackbar;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
-import android.util.Log;
 import android.view.Menu;
 import android.view.MenuItem;
 import android.view.View;
